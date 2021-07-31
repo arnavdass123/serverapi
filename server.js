91ef07197985b0fc0c67fb250c0fd3ab3cddd83c
arnav <arnavdass123@gmail.com>
2021-07-31 23:17:20 +0530

    i am adding my first heroku cloud

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const Joi = require("joi");
+const app = express();
+
+const courses = [
+  {
+    id: 1,
+    name: "y2mate.com - Ed Sheeran  Shape of You Official Video_v720P.mp4",
+  },
+  { id: 2, name: "y2mate.com - NO EXCUSES  Best Motivational Video_1080p.mp4" },
+  { id: 3, name: "y2mate.com - Shawn Mendes  24 Hours Lyric Video_1080p.mp4" },
+];
+
+let videopath ;
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("the course with the given ID was not found");
+  }
+  videopath = course.name;
+
+  res.sendFile(__dirname + "/hello.html");
+});
+
+app.get("/video", (req, res) => {
+  console.log(videopath);
+  require("./packege")(req, res, videopath);
+  // Ensure there is a range given for the video
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(req.params);
+});
+
+// port
+const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`listening on ${port}`));
