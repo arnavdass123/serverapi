051d8b45afb26067227d36ed7b211cc7567e1d6b
arnav <arnavdass123@gmail.com>
2021-08-01 11:10:17 +0530

    adding somthing

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ const courses = [
   },
   { id: 2, name: "y2mate.com - NO EXCUSES  Best Motivational Video_1080p.mp4" },
   { id: 3, name: "y2mate.com - Shawn Mendes  24 Hours Lyric Video_1080p.mp4" },
+  { id: 4, name: "hello bhai kaisa hai" },
+  { id: 5, name: "ok ab mujhe nind aa rahi hai " },
+  { id: 6, name: "ok data humare pass hai" },
+  { id: 7, name: "is 24 hours is hours" },
+  { id: 8, name: " ha bhai kaisa hai" },
+  { id: 9, name: "ok bhai kaisa hai thik humain" },
+  { id: 10, name: "acha mmain ja raha hu " },
+  { id: 11, name: "ok thik hai main soch raha hu" },
+  { id: 12, name: "yar mere pass nahi aa " },
+  { id: 13, name: "thik fir main kaya karu bata de" },
 ];
 
 app.get("/api/courses", (req, res) => {
